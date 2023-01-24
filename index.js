0e65252da4c2009e6d855042ddacaec1ffcbbdb1
anowarzz <anowarhosen444@gmail.com>
2023-01-24 22:11:43 +0600

    server connected with mongodb

--- index.js
+++ index.js
@@ -16,13 +16,24 @@ app.use(express.json());
 
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v1rp4a3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
 
 async function run () {
 
+try{
 
+const productCollection = client.db("ecommerceDashboard").collection("products")
+}
+
+finally{
+
+
+}
 }
 
 run().catch((error) => console.log(error));
